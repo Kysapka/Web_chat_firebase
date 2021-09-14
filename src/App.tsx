9dcca7c0f8bd41fb7e2275cf037b8a969c53c90b
Kysapka <kysapka@gmail.com>
2021-09-14 10:13:02 +0300

    Routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
     return (
-        <div className="App">
-            APP WEB CHAT
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <AppRouter />
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
+import {privateRoutes, publicRoutes} from "./routes";
+import {CHAT_ROUTE, LOGIN_ROUTE} from "./utils/consts";
+
+const AppRouter = () => {
+    const user = false
+    return user ?
+        (
+            <Switch>
+                {privateRoutes.map(({path, Component}) =>
+                <Route path={path} component={Component} exact={true}/>
+                )}
+                <Redirect to={CHAT_ROUTE} />
+            </Switch>
+        )
+        :
+        (
+            <Switch>
+                {publicRoutes.map(({path, Component}) =>
+                    <Route path={path} component={Component} exact={true}/>
+                )}
+                <Redirect to={LOGIN_ROUTE} />
+            </Switch>
+        )
+};
+
+export default AppRouter;
